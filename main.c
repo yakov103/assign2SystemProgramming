a22b5ccbaace508f0a4e8f1baf637e354d7d8746
dolev146 <dolev146@gmail.com>
2021-11-20 04:46:10 -0800

    minor fixes

--- main.c
+++ main.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include "my_mat.h"
 
+int main()
+{
+    char input = 1;
+    while (input != 'D')
+    {
+        scanf(" %c", &input);
+        switch (input)
+        {
+        case 'A':
+            inputData();
+            break;
+        case 'B':
+            checkEdges();
+            break;
+        case 'C':
+            bestRoutes();
+            break;
 
-int main() {
-    int mat[size][size];
-    int flag= False;
-    char input;
-    while (!flag){
-        scanf("%c", &input);
-        switch (input){
-            case 'A':
-                inputData(mat);
-                break;
-            case 'B':
-                checkEdges(mat);
-                break;
-            case 'C':
-                bestRoutes(mat);
-                break;
-
-            case 'D':
-                flag = True;
-                break;
-            default:
-                break;
-
-
+        case 'D':
+            break;
+        default:
+            break;
         }
-
     }
     return 0;
 }
--- my_mat.c
+++ my_mat.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include "my_mat.h"
 
+int mat[size][size];
+
 //1
-int inputData(int mat[size][size])
+int inputData()
 {
     int input;
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            scanf(" %d", &input);
+            scanf("%d", &input);
             mat[i][j] = input;
         }
     }
-    ApplyFloydAlgo(mat);
+    ApplyFloydAlgo();
     return 0;
 }
 
 //2
-int checkEdges(int mat[size][size])
+int checkEdges()
 {
     int i, j;
-    scanf("%d %d", &i, &j);
+    scanf("%d%d", &i, &j);
     if (mat[i][j] != 0)
     {
         printf("True\n");
@@ -35,10 +37,10 @@ int checkEdges(int mat[size][size])
 
 //3
 
-int bestRoutes(int mat[size][size])
+int bestRoutes()
 {
     int x, y;
-    scanf("%d %d", &x, &y);
+    scanf("%d%d", &x, &y);
     if (mat[x][y] == 0)
     {
         printf("-1\n");
@@ -50,7 +52,7 @@ int bestRoutes(int mat[size][size])
     return 0;
 }
 
-int ApplyFloydAlgo(int mat[size][size])
+int ApplyFloydAlgo()
 {
         for (int k = 0; k < size; k++) {
         for (int i = 0; i < size; i++) {
